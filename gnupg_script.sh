02e6670ff23c51ab222c96b74dce287b1d9a607e
JonathanMullo <jomullopa@uide.edu.ec>
2026-03-20 18:06:39 +0000

    export

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,4 @@ uname -a #daba información del navegador
 which gpg #nos da la ubicación del binario de gpg
 gpg --version #me da la versión e información adicional
 gpg --full-generate-key #genero una llave publica y privada
+gpg --armor --export #exportaba una lave publica a un archivo
