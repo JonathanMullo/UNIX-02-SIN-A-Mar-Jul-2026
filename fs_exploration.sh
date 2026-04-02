510e8f126baec497c438d7622b67b288be337ebc
JonathanMullo <jomullopa@uide.edu.ec>
2026-04-02 12:57:47 +0000

    wip: sudo apt install

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,9 @@
 cd /
 # Lists files and directories with a symbol at the end indicating their type
 ls -F
+# Update the package list
+sudo apt update
+# Update system packages
+sudo apt upgrade
+# Install the latest versions
+sudo apt install parted
