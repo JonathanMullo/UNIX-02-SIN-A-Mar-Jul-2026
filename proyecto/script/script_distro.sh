d934077de370ba4df62fb54c6e936d22c9df7e45
JonathanMullo <jomullopa@uide.edu.ec>
2026-04-09 14:02:25 +0000

    Add installation script and install required packages

---
+++ proyecto/script/script_distro.sh
@@ -0,0 +1,6 @@
+1._ Sudo apt update
+2._ sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
+3._ chmod +x script_distro.sh
+4._ ./script_distro.sh
+5._ git --version
+6._ gcc --version
