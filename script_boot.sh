1802427a1860544eb908cbad991891258820f9c4
JonathanMullo <jomullopa@uide.edu.ec>
2026-04-09 12:27:20 +0000

    absolute directory

---
+++ script_boot.sh
@@ -0,0 +1,11 @@
+# Create a new folder called project
+mkdir
+#Lists the files and folders in the current directory
+ls
+#Enter the project folder using a relative path
+cd proyecto /
+# Enter (change the current directory) to the project folder
+# Return to the previous directory
+cd ..
+# Move to that specific path (absolute directory)
+cd //workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
