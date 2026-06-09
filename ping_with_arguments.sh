b88f2b16c0473392bcea538a185d291d29d1c04b
JonathanMullo <jomullopa@uide.edu.ec>
2026-06-09 16:54:42 +0000

    wip: Black Hat Bash: Input Prompting

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -8,8 +8,14 @@ echo "Pinging the target: ${TARGET}..."
 ping "${TARGET}"
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
-
 # Change "$@" to "$*" to observe behavior.
 for args in "$@"; do
  echo "${args}"
 done
+
+echo "What is your first name?"
+read -r firstname
+echo "What is your last name?"
+read -r lastname
+echo "Your first name is ${firstname} and your last name is
+${lastname}"
