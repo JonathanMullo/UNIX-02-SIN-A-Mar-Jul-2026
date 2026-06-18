726865edde6759bf437594f9da4d2c5fcdf39db7
JonathanMullo <jomullopa@uide.edu.ec>
2026-06-18 12:44:08 +0000

    wip: Black Hta Bash: Filtering with grep 3

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -2,5 +2,10 @@
 grep "35.237.4.214" log.txt
 grep "35.237.4.214\|13.66.139.0" log.txt
 grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+#ps
 ps | grep TTY
 ps | grep -i tty
+# -v
+grep -v "35.237.4.214" log.txt
+# -o
+grep -o "35.237.4.214" log.txt
